59dd29f3e95a3e8102841429886ae20f82098cd0
Hirofumi Hida <Hirofumi@Hirofumis-MacBook-Air.local>
2017-06-23 19:24:44 +0900

    add func to upload aws s3 directly

--- global-ip.py
+++ global-ip.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
-import os, requests, subprocess, boto3
+import os, requests, subprocess, boto3, json, time
 from jinja2 import Environment, FileSystemLoader
 from datetime import datetime
 from json import load
 from urllib2 import urlopen
+
+bucket_name = "hirofumi-globalip"
+timestr = time.strftime("%Y%m%d-%H%M%S")
 
 globalip = load(urlopen('http://jsonip.com'))['ip']
 datetimeip = datetime.now()
@@ -19,16 +22,16 @@ def render_template(template_filename, context):
     return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
 
 def create_json():
-    #fname = "output.json"
     datalist = [globalip, datetimeip]
     context = {
         'ipdata': datalist
     }
-    print render_template('global-ip.json', context)
-    #
-    #with open(fname, 'w') as f:
-    #    ipdata = render_template('global-ip.json', context)
-    #    f.write(ipdata)
+    json_key = "global-ip-" + timestr + ".json"
+    s3 = boto3.resource('s3')
+    obj = s3.Object(bucket_name,json_key)
+    test_json = render_template('global-ip.json', context)
+    r = obj.put(Body = json.dumps(test_json))
+    #print render_template('global-ip.json', context)
  
 def main():
     create_json()
