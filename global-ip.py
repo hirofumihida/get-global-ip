396761e3096d86fd3088b09752520e933e7dfe7e
Hirofumi Hida <hirofumi.hida@gmail.com>
2017-06-30 10:18:42 +0000

    use os env for access_key and _id, convert to JST

--- global-ip.py
+++ global-ip.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 
-import os, requests, subprocess, boto3, json, time
+import os, requests, subprocess, boto3, json, time, pytz
 from jinja2 import Environment, FileSystemLoader
 from datetime import datetime
 from json import load
 from urllib2 import urlopen
 
+ACCESS_ID=os.environ['ACCESS_ID']
+ACCESS_KEY=os.environ['ACCESS_KEY']
+
 bucket_name = "hirofumi-globalip"
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
 globalip = load(urlopen('http://jsonip.com'))['ip']
-datetimeip = datetime.now()
+jst = pytz.timezone('Asia/Tokyo')
+datetimeip = datetime.now(jst)
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_ENVIRONMENT = Environment(
@@ -27,7 +31,9 @@ def create_json():
         'ipdata': datalist
     }
     json_key = "global-ip-" + timestr + ".json"
-    s3 = boto3.resource('s3')
+    s3 = boto3.resource('s3',
+         aws_access_key_id=ACCESS_ID,
+         aws_secret_access_key=ACCESS_KEY)
     obj = s3.Object(bucket_name,json_key)
     test_json = render_template('global-ip.json', context)
     r = obj.put(Body = json.dumps(test_json))
